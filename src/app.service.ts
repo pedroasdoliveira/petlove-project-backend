763cea74980eede043d3a671f1debb3f7ef4156f
Felipe <97033128+Felipe360flp@users.noreply.github.com>
2022-09-12 21:40:09 -0300

    Feat Initial Commit - NestJS

---
+++ src/app.service.ts
@@ -0,0 +1,8 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class AppService {
+  getAppStatus(): string {
+    return 'Server is running! 🚀\n Please check http://localhost:3333/api for Swagger docs...';
+  }
+}
