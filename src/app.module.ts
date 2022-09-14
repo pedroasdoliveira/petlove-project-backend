90c15ddb5ea64153ae72b40c98102986e7b89e91
Felipe <97033128+Felipe360flp@users.noreply.github.com>
2022-09-14 20:46:46 -0300

    Chore App Module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,6 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { UserModule } from './user/user.module';
-import { UserModule } from './user/user.module';
 
 @Module({
   imports: [PrismaModule, UserModule],
