5901a45b48955a36cbee4503564cf021da1fc5f9
Felipe <97033128+Felipe360flp@users.noreply.github.com>
2022-09-14 20:30:23 -0300

    Refactor main (Swagger)

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,30 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3333);
+
+  app.useGlobalPipes(new ValidationPipe());
+
+  const config = new DocumentBuilder()
+    .setTitle('API-PetLove')
+    .setDescription('BootCamp')
+    .setVersion('1.0.0')
+    .addTag('status')
+    .addTag('Auth')
+    .addTag('User')
+    .addTag("Test")
+    .addTag("Specialtie")
+    .addTag("Result")
+    //.addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(process.env.PORT || 3333);
 }
 bootstrap();
+
