921ff9f838d1030087926b346f7e77d878503533
Felipe <97033128+Felipe360flp@users.noreply.github.com>
2022-09-14 21:22:49 -0300

    fix user Controller

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -2,6 +2,7 @@ import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/commo
 import { UserService } from './user.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { User } from './entities/user.entity';
 
 @Controller('user')
 export class UserController {
