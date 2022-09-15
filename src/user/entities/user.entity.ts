e9ed6780f7dab48e7a5cdf3eee1666b0b68f2d05
Felipe <97033128+Felipe360flp@users.noreply.github.com>
2022-09-14 21:02:37 -0300

    Feat User Entity

--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,1 +1,13 @@
-export class User {}
+export class User {
+id?:string;
+name:string;
+email:string;
+password:string;
+team:string;
+chapter:string;
+role:string;
+isAdmin:boolean;
+//results:Result[];
+createdAt:Date;
+updatedAt:Date;
+}
