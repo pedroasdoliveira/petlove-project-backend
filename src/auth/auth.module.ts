e8933d07be6314389bc3876ae48acd8e9e014bcd
Bruna Bomfim <brunabomfim.bbb@gmail.com>
2022-09-16 20:43:01 -0300

    chore: JWT sign

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,9 +2,15 @@ import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { PrismaModule } from 'src/prisma/prisma.module';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [
+    PrismaModule,
+    JwtModule.register({
+      secret: process.env.JWT_SECRET,
+      signOptions: { expiresIn: '24h' }
+    })],
   controllers: [AuthController],
   providers: [AuthService]
 })
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,13 +1,34 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { PrismaService } from '../prisma/prisma.service'
 import { LoginDto } from './dto/login.dto';
 import { LoginResponseDto } from './dto/login.response.dto';
 
 @Injectable()
 export class AuthService {
+  constructor(
+    private readonly prisma: PrismaService,
+    private readonly jwtService: JwtService)
+  {}
+
   async login(loginDto: LoginDto): Promise<LoginResponseDto> {
+    const { email, password } = loginDto;
+
+    const user = await this.prisma.user.findUnique({ where: { email } });
+
+    if (!user){
+      throw new UnauthorizedException('Invalid email and/or password')
+    }
+
+    //const isHashValid = await bcrypt.compare(password, user.password);
+    /*if (!isHashValid) {
+      throw new UnauthorizedException('Invalid email and/or password')
+    }*/
+
+    delete user.password;
     return {
-      token: 'teste',
-      user: undefined
+      token: this.jwtService.sign({ email }),
+      user
     }
   }
 }
