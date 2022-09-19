48649a83f46e617f1561346642fdd17449e08d8e
Felipe <97033128+Felipe360flp@users.noreply.github.com>
2022-09-19 19:36:20 -0300

    Refactor: comments

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -18,14 +18,14 @@ export class UserController {
 
   @ApiTags('User')
   @Get()
-  @ApiOperation({ summary: 'Listar todos os usuários.' }) // Pendente autenticação
+  @ApiOperation({ summary: 'Listar todos os usuários.' }) //Pendente isAdmin e autenticação
   findAll() {
     return this.userService.findAll();
   }
 
   @ApiTags('User')
   @Get(':id')
-  @ApiOperation({ summary: 'Visualizar um usuário pelo id.' }) // Pendente autenticação
+  @ApiOperation({ summary: 'Visualizar um usuário pelo id.' }) //Pendente autenticação
   findOne(@Param('id') id: string) {
     return this.userService.findOne(id);
   }
@@ -33,7 +33,7 @@ export class UserController {
   @ApiTags('User')
   @Patch(':id')
   @ApiOperation({ summary: 'Editar informações de usuário!' })
-  update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) { // Pendente autenticação
+  update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) { //Pendente isAdmin e autenticação
     return this.userService.update(id, updateUserDto);
   }
 
