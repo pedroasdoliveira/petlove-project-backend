15ca18b2e4de7287d992c9707695930196522aa5
Felipe <97033128+Felipe360flp@users.noreply.github.com>
2022-09-19 19:50:03 -0300

    Refactor: user controller

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -5,39 +5,35 @@ import { UpdateUserDto } from './dto/update-user.dto';
 import { User } from './entities/user.entity';
 import { ApiOperation, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('User')
 @Controller('User')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
-  @ApiTags('User')
   @Post('create')
   @ApiOperation({ summary: 'Criar usuário' })
   create(@Body() createUserDto: CreateUserDto) {
     return this.userService.create(createUserDto);
   }
 
-  @ApiTags('User')
   @Get()
   @ApiOperation({ summary: 'Listar todos os usuários.' }) //Pendente isAdmin e autenticação
   findAll() {
     return this.userService.findAll();
   }
 
-  @ApiTags('User')
   @Get(':id')
   @ApiOperation({ summary: 'Visualizar um usuário pelo id.' }) //Pendente autenticação
   findOne(@Param('id') id: string) {
     return this.userService.findOne(id);
   }
 
-  @ApiTags('User')
   @Patch(':id')
   @ApiOperation({ summary: 'Editar informações de usuário!' })
   update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) { //Pendente isAdmin e autenticação
     return this.userService.update(id, updateUserDto);
   }
 
-  @ApiTags('User')
   @Delete(':id')
   @ApiOperation({ summary: 'Deletar um usuário (Adm)' })
   remove(@Param('id') id: string) {
