36037141216ced9320b148673a9712f1ee44b933
Bruna Bomfim <brunabomfim.bbb@gmail.com>
2022-09-19 22:39:21 -0300

    chore: JWT strategy

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,5 +1,14 @@
-import { Body, Controller, Post, HttpCode, HttpStatus } from '@nestjs/common';
-import { ApiOperation, ApiTags } from '@nestjs/swagger';
+import {
+  Body,
+  Controller,
+  Post,
+  HttpCode,
+  HttpStatus,
+  Get,
+  UseGuards,
+} from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport'
+import { ApiBearerAuth, ApiOperation, ApiTags } from '@nestjs/swagger';
 import { AuthService } from './auth.service';
 import { LoginDto } from './dto/login.dto';
 import { LoginResponseDto } from './dto/login.response.dto';
@@ -12,9 +21,19 @@ export class AuthController {
   @Post()
   @HttpCode(HttpStatus.OK)
   @ApiOperation({
-    summary: "Login receiving a token for authentication"
+    summary: 'Login receiving a token for authentication',
   })
   login(@Body() loginDto: LoginDto): Promise<LoginResponseDto> {
-    return this.authService.login(loginDto)
+    return this.authService.login(loginDto);
+  }
+
+  @Get()
+  @UseGuards(AuthGuard())
+  @ApiOperation({
+    summary: 'Returns the currently authenticated user'
+  })
+  @ApiBearerAuth()
+  profile() {
+    return { message: 'Successful authentication' };
   }
 }
