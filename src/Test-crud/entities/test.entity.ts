917c637529acb6e552089576cf723d43044cede3
Felipe <97033128+Felipe360flp@users.noreply.github.com>
2022-09-21 22:01:10 -0300

    Refactor: Prisma Schema and test entity

--- src/Test-crud/entities/test.entity.ts
+++ src/Test-crud/entities/test.entity.ts
@@ -2,7 +2,9 @@ export class Test {
 id?:string;
 system:string[];
 person:string[];
-technology:string[];
+test:string[];
+design:string[];
+toolshop:string[];
 influence:string[];
 process:string[];
 createdAt:Date;
