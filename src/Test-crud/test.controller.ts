491dfb4f8c063d6aefc66cc91da455aba5ad82b2
Felipe <97033128+Felipe360flp@users.noreply.github.com>
2022-09-23 19:16:24 -0300

    Refactor: Test Crud, service and controller

--- src/Test-crud/test.controller.ts
+++ src/Test-crud/test.controller.ts
@@ -5,10 +5,11 @@ import { UpdateTestDto } from './dto/update-test.dto';
 import { ApiBearerAuth, ApiOperation, ApiTags } from '@nestjs/swagger';
 import { LoggedUser } from 'src/auth/logged-user.decorator';
 import { User } from 'src/user/entities/user.entity';
-import { domainToASCII } from 'url';
 import { AuthGuard } from '@nestjs/passport';
 
 @ApiTags('Test')
+@UseGuards(AuthGuard())
+@ApiBearerAuth()
 @Controller('Test')
 export class TestController {
   constructor(private readonly testService: TestService) {}
@@ -19,30 +20,24 @@ export class TestController {
     return this.testService.create(dto,user);
   }
 
-  @UseGuards(AuthGuard())
-  @ApiBearerAuth()
   @Get('/allTests')
   @ApiOperation({summary: `Para listar todos os testes`})
   findAll(@LoggedUser() user:User) {
     return this.testService.findAll(user);
   }
 
-  @UseGuards(AuthGuard())
-  @ApiBearerAuth()
+
   @Get(':id')
   findOne(@Param('id') id: string,@LoggedUser() user:User) {
     return this.testService.findOne(id,user);
   }
 
-  @UseGuards(AuthGuard())
-  @ApiBearerAuth()
   @Patch(':id')
   update(@Param('id') id: string, @Body() dto: UpdateTestDto,@LoggedUser() user:User) {
     return this.testService.update(id,dto,user);
   }
 
-  @UseGuards(AuthGuard())
-  @ApiBearerAuth()
+
   @Delete(':id')
   remove(@Param('id') id:string,@LoggedUser() user:User) {
     return this.testService.remove(id,user);
