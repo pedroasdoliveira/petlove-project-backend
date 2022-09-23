44e4f070d39e2e2d2841e2b3f105da86e792b61f
Bruna Bomfim <brunabomfim.bbb@gmail.com>
2022-09-23 20:26:58 -0300

    chore: Speciaties entities and dto

--- src/specialties/dto/create-specialty.dto.ts
+++ src/specialties/dto/create-specialty.dto.ts
@@ -1,1 +1,46 @@
-export class CreateSpecialtyDto {}
+import { ApiProperty } from "@nestjs/swagger";
+import { IsNumber, IsString } from "class-validator";
+
+export class CreateSpecialtyDto {
+
+  @IsString()
+  @ApiProperty({
+    description: 'Professional level',
+    example: 'Junior',
+  })
+  performance: string;
+
+  @IsNumber()
+  @ApiProperty({
+    description: 'Systems score',
+  })
+  system: number;
+
+  @IsNumber()
+  @ApiProperty({
+    description: 'Persons score',
+  })
+  person: number;
+
+  @IsNumber()
+
+  @ApiProperty({
+    description: 'Tecnology score',
+  })
+  technology: number;
+
+  @IsNumber()
+  @ApiProperty({
+    description: 'Process score',
+  })
+  process: number;
+
+  @IsNumber()
+  @ApiProperty({
+    description: 'Influence score',
+  })
+  influence: number;
+
+  createdAt: Date;
+  updatedAt: Date;
+}
--- src/specialties/entities/specialty.entity.ts
+++ src/specialties/entities/specialty.entity.ts
@@ -1,1 +1,10 @@
-export class Specialty {}
+  id: string;
+  performance: string;
+  system: number;
+  person: number;
+  technology: number;
+  process: number;
+  influence: number;
+  createdAt: Date;
+  updatedAt: Date;
+}
