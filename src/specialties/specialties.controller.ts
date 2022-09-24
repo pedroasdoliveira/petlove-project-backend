48829b0f072752c94c7ef41d1f8e153a26f9bd19
Bruna Bomfim <brunabomfim.bbb@gmail.com>
2022-09-23 21:59:43 -0300

    chore: CRUD Specialtie, LoggedUser and isAdmin

--- src/specialties/specialties.controller.ts
+++ src/specialties/specialties.controller.ts
@@ -1,34 +1,54 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  UseGuards,
+} from '@nestjs/common';
 import { SpecialtiesService } from './specialties.service';
 import { CreateSpecialtyDto } from './dto/create-specialty.dto';
 import { UpdateSpecialtyDto } from './dto/update-specialty.dto';
+import { User } from 'src/user/entities/user.entity';
+import { LoggedUser } from 'src/auth/logged-user.decorator';
+import { AuthGuard } from '@nestjs/passport';
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
 
-@Controller('specialties')
+@UseGuards(AuthGuard())
+@ApiBearerAuth()
+@ApiTags('Specialtie')
+@Controller('specialty')
 export class SpecialtiesController {
-  constructor(private readonly specialtiesService: SpecialtiesService) {}
+  constructor(private readonly specialty: SpecialtiesService) {}
 
   @Post()
-  create(@Body() createSpecialtyDto: CreateSpecialtyDto) {
-    return this.specialtiesService.create(createSpecialtyDto);
+  create(@Body() dto: CreateSpecialtyDto, @LoggedUser() user: User) {
+    return this.specialty.create(dto, user);
   }
 
   @Get()
-  findAll() {
-    return this.specialtiesService.findAll();
+  findAll(@LoggedUser() user: User) {
+    return this.specialty.findAll(user);
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.specialtiesService.findOne(+id);
+  findOne(@Param('id') id: string, @LoggedUser() user: User) {
+    return this.specialty.findOne(id, user);
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateSpecialtyDto: UpdateSpecialtyDto) {
-    return this.specialtiesService.update(+id, updateSpecialtyDto);
+  update(
+    @Param('id') id: string,
+    @Body() dto: UpdateSpecialtyDto,
+    @LoggedUser() user: User,
+  ) {
+    return this.specialty.update(id, dto, user);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.specialtiesService.remove(+id);
+  remove(@Param('id') id: string, @LoggedUser() user: User) {
+    return this.specialty.remove(id, user);
   }
 }
