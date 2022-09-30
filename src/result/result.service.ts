5733c903d613f6925b8f5fed7ac58ed9e3e54e53
Bruna Bomfim <brunabomfim.bbb@gmail.com>
2022-09-29 21:27:31 -0300

    fix: prisma service

--- src/result/result.service.ts
+++ src/result/result.service.ts
@@ -1,10 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
 import { handleError } from 'src/utils/handleError.utils';
 import { CreateResultDto } from './dto/create-result.dto';
 import { UpdateResultDto } from './dto/update-result.dto';
 
 @Injectable()
 export class ResultService {
+  constructor(private readonly prisma: PrismaService) {}
+
   create(createResultDto: CreateResultDto) {
     return 'This action adds a new result';
   }
@@ -13,15 +16,15 @@ export class ResultService {
     return `This action returns all result`;
   }
 
-  findOne(id: number) {
+  findOne(id: string) {
     return `This action returns a #${id} result`;
   }
 
-  update(id: number, updateResultDto: UpdateResultDto) {
+  update(id: string, updateResultDto: UpdateResultDto) {
     return `This action updates a #${id} result`;
   }
 
-  async delete(id: number) {
+  async delete(id: string) {
     await this.prisma.result.delete({ where: {id} }).catch(handleError)
     return { message: 'Results deleted successfully'};
   }
