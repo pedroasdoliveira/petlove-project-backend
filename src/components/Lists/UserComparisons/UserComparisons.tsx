0ed47d1fa1d1e184727073c5bb5cc84e3fe9d50b
Giovanne Berteli Comba <77624454+hethus@users.noreply.github.com>
2022-10-24 11:11:11 -0300

    style: :fire: removed lines

--- src/components/Lists/UserComparisons/UserComparisons.tsx
+++ src/components/Lists/UserComparisons/UserComparisons.tsx
@@ -320,7 +320,6 @@ const UserComparisons = () => {
                   >
                     <Tooltip
                       label={`Função com mais devs na equipe, ou seja, devs na função majoritária`}
-                      aria-label=""
                       hasArrow
                     >
                       {`${
@@ -332,14 +331,12 @@ const UserComparisons = () => {
 
                     <Tooltip
                       label={`quantidade de devs na função majoritária`}
-                      aria-label=""
                       hasArrow
                     >
                       {`(${specialityFiltered[0][1]})`}
                     </Tooltip>
                     <Tooltip
                       label={`devs na função majoritária que fizeram pelo menos um teste`}
-                      aria-label=""
                       hasArrow
                     >
                       {`(${teamLength})`}
