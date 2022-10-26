4890d349d817333a3922cb95a699138333872067
Giovanne Berteli Comba <77624454+hethus@users.noreply.github.com>
2022-10-26 18:17:50 -0300

    fix: :rocket: fixed stuff to deploy

--- src/prisma/seed/03-specialties.ts
+++ src/prisma/seed/03-specialties.ts
@@ -2,13 +2,64 @@ import { Prisma, PrismaClient } from "@prisma/client";
 
 export const specialties: Prisma.SpecialtieCreateInput[] = [
   {
-    performance: "Inserir nome",
-    description: "Inserir descrição",
-    system: 0,
-    person: 0,
-    technology: 0,
-    process: 0,
-    influence: 0,
+    performance: "Trainee",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. - Trainee",
+    system: 1,
+    person: 2,
+    technology: 0.8,
+    process: 1,
+    influence: 1.4,
+  },
+  {
+    performance: "Junior",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. - Junior",
+    system: 1.8,
+    person: 2.3,
+    technology: 2.6,
+    process: 1.6,
+    influence: 2,
+  },
+  {
+    performance: "Pleno",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. - Pleno",
+    system: 3.1,
+    person: 2.4,
+    technology: 3.1,
+    process: 2.3,
+    influence: 2.6,
+  },
+  {
+    performance: "Senior",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. - Senior",
+    system: 3.6,
+    person: 2.7,
+    technology: 3.4,
+    process: 2.3,
+    influence: 2.8,
+  },
+  {
+    performance: "Especialista",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. - Especialista",
+    system: 4.7,
+    person: 2.2,
+    technology: 4.2,
+    process: 2.3,
+    influence: 2.8,
+  },
+  {
+    performance: "Tech-Lead",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. - Tech-Lead",
+    system: 3.8,
+    person: 3.6,
+    technology: 3.7,
+    process: 3.8,
+    influence: 3.7,
   },
 ];
 
