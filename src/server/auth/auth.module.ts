c6edb5f584f7d7fd871e72b6a20ae99faec8637f
Pedro Augusto Silva de Oliveira <89483941+pedroasdoliveira@users.noreply.github.com>
2022-10-26 00:54:06 -0300

    refactor: make changes

--- src/server/auth/auth.module.ts
+++ src/server/auth/auth.module.ts
@@ -1,10 +1,10 @@
 import { Module } from "@nestjs/common";
 import { AuthService } from "./auth.service";
 import { AuthController } from "./auth.controller";
-import { PrismaModule } from "src/server/prisma/prisma.module";
 import { JwtModule } from "@nestjs/jwt";
 import { PassportModule } from "@nestjs/passport";
 import { JwtStrategy } from "./jwt.strategy";
+import { PrismaModule } from "../prisma/prisma.module";
 
 @Module({
   imports: [
